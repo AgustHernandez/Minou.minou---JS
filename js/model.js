b55f31cd4f13e7dadf46e11b4e7d549b5a929335
AgustHernandez <hagustinadaniela@gmail.com>
2021-12-16 14:08:54 -0300

    Correción JS

--- js/model.js
+++ js/model.js
@@ -1,29 +1,3 @@
-class Producto {
-    constructor (code,nombre, precio, stock, nombreImg, nombreHTML) {
-        this.code = code;
-        this.nombre = nombre;
-        this.precio = parseInt(precio);
-        this.stock = stock;
-        this.IVA = 0.21;
-        this.nombreImg = nombreImg
-        this.nombreHTML = nombreHTML
-    }
-
-    valorIVA(){
-        return this.precio * this.IVA;
-    }
-
-    mostrarConIva()
-    {
-        return this.precio + this.valorIVA();
-    }
-
-    //Actualiza el stock en base a lo vendido
-    actualizarStock(cantVendida) {
-        this.stock += cantVendida
-    }
-}
-
 class Carrito {
     constructor () {
         this.productos = []
