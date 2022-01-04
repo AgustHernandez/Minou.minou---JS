f71f98c407fc8caaa0fd2390cf8ae09ad4be28e2
AgustHernandez <hagustinadaniela@gmail.com>
2022-01-04 20:08:47 -0300

    Modificación sección producto

--- js/eventos.js
+++ js/eventos.js
@@ -55,7 +55,80 @@ function imprimirProducto(producto) {
                                 <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
                             </svg>
                         </div>
-                        <p class="fs-5">Calculá el costo <br> de tu envio</p>
+                        <p class="fs-5" data-bs-toggle="modal" data-bs-target="#calculaEnvio">Calculá el costo <br> de tu envio</p>
+                    </div>
+                    <div class="modal fade" id="calculaEnvio" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h3 class="modal-title fs-3" id="exampleModalLabel">Costo de Envío</h3>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body" id="modal-body-envio">
+                                    <h4 class="mb-3"><b>Según domicilio:</b></h4>
+                                    <div class="col-lg-12 costoEnvios">
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Entrega en CABA</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">GRATIS !</p>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Entrega en GBA y Resto del país</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">$ 200</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h4 class="mb-3"><b>Según tiempo de entrega:</b></h4>
+                                    <div class="col-lg-12 costoEnvios">
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Entrega en 24 hs.</b><br>Llega entre hoy y 1 día hábil</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">$ 500</p>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Entrega en 72 hs.<br>Llega entre 1 y 3 días hábiles</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">$ 200</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h4 class="mb-3"><b>Según total de la compra:</b></h4>
+                                    <div class="col-lg-12 costoEnvios">
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Compras menores a $ 1.000</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">$ 300</p>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Compras menores a $ 5.000</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">$ 200</p>
+                                            </div>
+                                        </div>
+                                        <hr>
+                                        <div class="align-text-bottom position-relative">
+                                            <p class="fs-5">Compras iguales o mayores a $ 5.000</p>
+                                            <div class="precioCostoEnvio">
+                                                <p class="textCostoEnvio">GRATIS !</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row ml-ato">
+                                    <p id="precioTotal"></p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div class="text-center wdth">
                         <div class="mb-3">
